986537e366c51c5c247d399c51c01ef1b762817e
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-05 20:54:31 +0700

    fix config

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,12 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-messaging.js');
 var config = {
-apiKey: "AIzaSyAXK4Orxl2CghIQvKiUPtkhEngSgzteqE0",
-authDomain: "hello-world-pwa-8669c.firebaseapp.com",
+apiKey: "AIzaSyAJF_AG26qNlMeZ9zCSWK8jSs1tLAIHCGs",
+authDomain: "my-pwa-app-a08c2.firebaseapp.com",
 databaseURL: "https://hello-world-pwa-8669c.firebaseio.com",
-projectId: "hello-world-pwa-8669c",
-storageBucket: "hello-world-pwa-8669c.appspot.com",
-messagingSenderId: "660239288739"
+projectId: "my-pwa-app-a08c2",
+storageBucket: "my-pwa-app-a08c2.appspot.com",
+messagingSenderId: "916709297858",
+appId: "1:916709297858:web:99f72c9973a1d935ccb55b",
 };
 firebase.initializeApp(config);
 const messaging = firebase.messaging();
