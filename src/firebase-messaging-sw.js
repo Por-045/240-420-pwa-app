eb26ffca37b9e5148f0dd6117fe389c39063e4b2
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-05 20:59:29 +0700

    databaseURl fix

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.4.0/firebase-messaging.js');
 var config = {
 apiKey: "AIzaSyAJF_AG26qNlMeZ9zCSWK8jSs1tLAIHCGs",
 authDomain: "my-pwa-app-a08c2.firebaseapp.com",
-databaseURL: "https://hello-world-pwa-8669c.firebaseio.com",
+databaseURL: "https://my-pwa-app-a08c2-default-rtdb.asia-southeast1.firebasedatabase.app",
 projectId: "my-pwa-app-a08c2",
 storageBucket: "my-pwa-app-a08c2.appspot.com",
 messagingSenderId: "916709297858",
